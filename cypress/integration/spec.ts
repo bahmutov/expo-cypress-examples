5bcb43ef64ad105bfc746ce29a09a8308b3a41ef
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-06-18 15:45:27 -0400

    add test for not found tab

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -3,11 +3,25 @@ describe('Expo app tabs', () => {
     cy.visit('/')
     // automatically goes to the first tab
     cy.location('pathname').should('equal', '/one')
-    cy.get('[role=heading]').should('have.text', 'Tab One Title')
-    cy.log('navigate to the second tab')
+    cy.contains('[role=heading]', 'Tab One Title')
+
+    cy.log('**going to the second tab**')
     cy.contains('[role=link]', 'TabTwo').click()
     cy.location('pathname').should('equal', '/two')
+    cy.contains('[role=heading]', 'Tab Two Title')
+
+    // alternative selector
     cy.get('[role=heading]:visible')
       .should('have.text', 'Tab Two Title')
+  })
+
+  it.only('goes to not found tab', () => {
+    cy.visit('/two')
+    cy.location('pathname').should('equal', '/two')
+
+    cy.visit('/unknown')
+    cy.location('pathname').should('equal', '/NotFound')
+    cy.contains('Go to home').click()
+    cy.location('pathname').should('equal', '/one')
   })
 })
